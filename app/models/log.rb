0ff4493795627798cc749a9310e1befd5360635e
takumanaruke <leon32taku@gmail.com>
2018-10-02 20:21:42 +0900

    added validation

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,12 @@
 class Log < ApplicationRecord
+  validates: :title, presence: true
+  validates: :date, presence: true
 end
+
+
+
+    # t.string "card_name"
+    # t.text "description"
+    # t.date "date"
+    # t.string "title"
+    # t.string "spread_memo"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has _many :logs
+  validates :user_name, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true, length: {in: 6..20}, on: :create
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
