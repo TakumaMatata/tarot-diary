98c0900ce48391d228ff0b4f55a808220f1c82ea
takumanaruke <leon32taku@gmail.com>
2018-10-02 20:28:00 +0900

    fixed a tyoo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has _many :logs
+  has_many :logs
   validates :user_name, presence: true
   validates :email, uniqueness: true, presence: true
   validates :password, presence: true, length: {in: 6..20}, on: :create
