efb8a1ca5e227ce601fbace75b9efdd6ea0792a1
takumanaruke <leon32taku@gmail.com>
2018-10-03 18:03:34 +0900

    added pundit and controllers

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -1,6 +1,48 @@
 class LogsController < ApplicationController
 
-  def create?
-    return true
+  def index
+    @logs = Log.all
+  end
+
+  def show
+    authorize @log
+    @log = Log.find(params[:id])
+  end
+
+  def new
+    @log = Log.new
+    authorize @log
+  end
+
+  def create
+    @log = Log.new(log_params)
+    authorize @log
+    if @log.save
+      redirect_to logs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def log_params
+    params.require(:log).permit(:title, :date, :spread_memo, :card_name, :description)
   end
 end
+
+    # t.string "card_name"
+    # t.text "description"
+    # t.date "date"
+    # t.string "title"
+    # t.string "spread_memo"
--- app/policies/log_policy.rb
+++ app/policies/log_policy.rb
@@ -1,7 +1,11 @@
-class LogsPolicy < ApplicationPolicy
+class LogPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       scope.all
     end
+  end
+
+  def create?
+    true
   end
 end
