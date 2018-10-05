5ec59fddb7c69719414cc5b34d1462b0b9756360
takumanaruke <leon32taku@gmail.com>
2018-10-05 14:44:38 +0900

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get 'pages/contact'
-  resources :users, only: [:index, :show, :edit, :update]
-  resources :logs
-
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :logs
+  end
 end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
